21f8c51fa235055e719a166687847a284636b633
michebble <32097294+michebble@users.noreply.github.com>
2021-07-22 14:31:45 +1000

    Update Move module
    We should not allow movement beyond the bounds of the board

--- lib/move.ex
+++ lib/move.ex
@@ -11,9 +11,20 @@ defmodule Move do
   ## Examples
       iex> Move.call(%{x: 0, y: 1, facing: :NORTH})
       %{x: 0, y: 2, facing: :NORTH}
+
+      iex> Move.call(%{x: 0, y: 1, facing: :WEST})
+      %{x: 0, y: 1, facing: :WEST}
   """
-  def call(%{x: x, facing: :EAST} = current_placement) do merge(current_placement, %{x: x + 1}) end
-  def call(%{y: y, facing: :NORTH} = current_placement) do merge(current_placement, %{y: y + 1}) end
-  def call(%{y: y, facing: :SOUTH} = current_placement) do merge(current_placement, %{y: y - 1}) end
-  def call(%{x: x, facing: :WEST} = current_placement) do merge(current_placement, %{x: x - 1}) end
+  def call(%{x: x, facing: :EAST} = current_placement) do x + 1 |> update_position(:x, current_placement) end
+  def call(%{y: y, facing: :NORTH} = current_placement) do y + 1 |> update_position(:y, current_placement) end
+  def call(%{y: y, facing: :SOUTH} = current_placement) do y - 1 |> update_position(:y, current_placement) end
+  def call(%{x: x, facing: :WEST} = current_placement) do x - 1 |> update_position(:x, current_placement) end
+
+  defp is_on_board(new_position, board_size \\ 0..4) do new_position in board_size end
+
+  defp update_position(new_position, axis, current_placement) do
+    if is_on_board(new_position),
+      do: merge(current_placement, %{axis => new_position}),
+      else: current_placement
+  end
 end
--- test/move_test.exs
+++ test/move_test.exs
@@ -17,4 +17,20 @@ defmodule MoveTest do
   test "moving west" do
     assert Move.call(%{x: 4, y: 4, facing: :WEST}) == %{x: 3, y: 4, facing: :WEST}
   end
+
+  test "moving north against boundary" do
+    assert Move.call(%{x: 0, y: 4, facing: :NORTH}) == %{x: 0, y: 4, facing: :NORTH}
+  end
+
+  test "moving south against boundary" do
+    assert Move.call(%{x: 4, y: 0, facing: :SOUTH}) == %{x: 4, y: 0, facing: :SOUTH}
+  end
+
+  test "moving east against boundary" do
+    assert Move.call(%{x: 4, y: 0, facing: :EAST}) == %{x: 4, y: 0, facing: :EAST}
+  end
+
+  test "moving west against boundary" do
+    assert Move.call(%{x: 0, y: 4, facing: :WEST}) == %{x: 0, y: 4, facing: :WEST}
+  end
 end
