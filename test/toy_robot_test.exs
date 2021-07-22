72cc1a112cf54d64a63604c2af659da538e0239a
michebble <32097294+michebble@users.noreply.github.com>
2021-07-22 13:00:24 +1000

    Update toy robot test
    We should list all examples to know when the problem is solved

--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -6,4 +6,12 @@ defmodule ToyRobotTest do
     commands = ["PLACE 0,0,NORTH","MOVE","REPORT"]
     assert ToyRobot.process_commands(commands) == "0,1,NORTH"
   end
+  test "example 2" do
+    commands = ["PLACE 0,0,NORTH", "LEFT","REPORT"]
+    assert ToyRobot.process_commands(commands) == "0,0,WEST"
+  end
+  test "example 3" do
+    commands = ["PLACE 1,2,EAST","MOVE","MOVE","LEFT","MOVE","REPORT"]
+    assert ToyRobot.process_commands(commands) == "3,3,NORTH"
+  end
 end
