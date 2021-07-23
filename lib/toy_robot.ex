81531fc6e80ae85a3d16bcb90196c53b0869a29f
michebble <32097294+michebble@users.noreply.github.com>
2021-07-23 10:04:01 +1000

    Add modules to ToyRobot
    We want toy robot to use these modules to give it behaviour.

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -9,10 +9,20 @@ defmodule ToyRobot do
   ## Examples
       iex> commands = ["PLACE 0,0,NORTH","MOVE","REPORT"]
       iex> ToyRobot.process_commands(commands)
-      "0,1,NORTH"
+      %{facing: :NORTH, x: 0, y: 1}
 
   """
-  def process_commands(_commands) do
-    "0,1,NORTH"
+
+  def process_commands(commands) do
+    inital_state = %{x: nil, y: nil, facing: nil}
+    Enum.reduce(commands, inital_state, fn command, state -> execute_commands(command, state) end)
+  end
+
+  defp execute_commands("LEFT", state) do Turn.call(state, "LEFT") end
+  defp execute_commands("RIGHT", state) do Turn.call(state, "RIGHT") end
+  defp execute_commands("MOVE", state) do Move.call(state) end
+  defp execute_commands("REPORT", state) do Report.call(state) end
+  defp execute_commands(command, _state) do
+    Place.call(command)
   end
 end
--- test/toy_robot_test.exs
+++ test/toy_robot_test.exs
@@ -1,17 +1,19 @@
 defmodule ToyRobotTest do
   use ExUnit.Case
   doctest ToyRobot
+  import ExUnit.CaptureIO
 
-  test "example 1" do
-    commands = ["PLACE 0,0,NORTH","MOVE","REPORT"]
-    assert ToyRobot.process_commands(commands) == "0,1,NORTH"
-  end
-  test "example 2" do
-    commands = ["PLACE 0,0,NORTH", "LEFT","REPORT"]
-    assert ToyRobot.process_commands(commands) == "0,0,WEST"
-  end
-  test "example 3" do
-    commands = ["PLACE 1,2,EAST","MOVE","MOVE","LEFT","MOVE","REPORT"]
-    assert ToyRobot.process_commands(commands) == "3,3,NORTH"
+  test "it accepts commands and outputs it final placement to the console" do
+    assert capture_io(fn ->
+      ToyRobot.process_commands(["PLACE 0,0,NORTH","MOVE","REPORT"])
+    end) == "0,1,NORTH\n"
+
+    assert capture_io(fn ->
+      ToyRobot.process_commands(["PLACE 0,0,NORTH", "LEFT","REPORT"])
+    end) == "0,0,WEST\n"
+
+    assert capture_io(fn ->
+      ToyRobot.process_commands(["PLACE 1,2,EAST","MOVE","MOVE","LEFT","MOVE","REPORT"])
+    end) == "3,3,NORTH\n"
   end
 end
